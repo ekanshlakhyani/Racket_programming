6a7b42f45bbb9544e7556491ff190d5a12baf003
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-08 22:43:49 -0400

    Add files via upload

---
+++ a07/a07q1.rkt
@@ -0,0 +1,57 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname a07q1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;;
+;; ******************************************
+;;   Ekansh Lakhyani (20945086)
+;;   CS 115 Fall 2021
+;;   Assignment 07, Question 1
+;; ******************************************
+;;
+
+
+;; (subtract-min lon) produces a list of numbers after subtracting
+;;  the minimum number of the given list of numbers lon from every
+;;  member of lon.
+;; subtract-min:(listof Num) -> (listof Num)
+
+;; Examples:
+(check-expect (subtract-min (list 6 3 2 4 1 9 7))
+              (list 5 2 1 3 0 8 6))
+(check-expect (subtract-min empty)
+              empty)
+
+(define (subtract-min lon)
+  (local
+    [
+     ;; (min-list n lon) produces the minimum value when the between the
+     ;;  given number n and the list of numbers lon.
+     ;; min-list: Num (listof Num) -> Num
+
+     (define (min-list n lon)
+       (cond
+         [(empty? lon) n]
+         [else (min n (min-list (first lon) (rest lon)))]))
+
+
+     ;; (subtracter n lon) produces a list of numbers after subtracting
+     ;;  the number n from every member of the given list of numbers lon. 
+     ;; subtracter: Num (listof Num) -> (listof Num)
+
+     (define (subtracter n lon)
+       (cond [(empty? lon) empty]
+             [else (cons (- (first lon) n) (subtracter n (rest lon)))]))]
+
+    (cond [(empty? lon) empty]
+          [else
+           (local
+             [(define minimum (min-list (first lon) (rest lon)))]
+             (subtracter minimum lon))])))   
+
+;; Tests
+(check-expect (subtract-min (list 3 3 -1 7.7)) (list 4 4 0 8.7))
+(check-expect (subtract-min (build-list 10000 add1)) 
+              (build-list 10000 identity))
+(check-expect (subtract-min (list 8 3 0 7)) (list 8 3 0 7))
+(check-expect (subtract-min (list 9 3 9 8)) (list 6 0 6 5))
+(check-expect (subtract-min (list -8 -3 0 -7)) (list 0 5 8 1))
---
+++ a07/a07q2.rkt
@@ -0,0 +1,52 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname a07q2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; ******************************************
+;;   Ekansh Lakhyani (20945086)
+;;   CS 115 Fall 2021
+;;   Assignment 07, Question 2
+;; ******************************************
+;;
+
+
+;; (lenlen n) produces the LenLen number consuming a natural number n. The
+;;  LenLen number can be defined as the sum of- the sum of digits of n,
+;;  LenLen number of LenLen(n-1), the number of digits of the LenLen number
+;;  of LenLen(n-1), the number of digits of the number of digits of the
+;;  LenLen number of LenLen(n-1).
+;; lenlen: Nat -> Nat
+
+;; Examples:
+(check-expect (lenlen 0) 1)
+(check-expect (lenlen 1) 2)
+(check-expect (lenlen 4) 18)
+
+(define (lenlen n)
+  (local
+    [
+     ;; (digsum n) produces the sum of digits of the natural number n.
+     ;; digsum: Nat -> Nat
+     (define (digsum n)
+       (cond [(= (quotient n 10) 0) n]
+             [else (+ (remainder n 10) (digsum (quotient n 10)))]))
+
+
+     ;; (len n) produces the number of digits of natural number n.
+     ;; len: Nat -> Nat
+     (define (len n) (string-length (number->string n)))]
+
+    (cond [(equal? n 0) 1]
+          [(equal? n 1) 2]
+          [else
+           (local
+             [(define lenlenn-1 (lenlen (- n 1)))]
+             (+ (digsum n)
+                lenlenn-1
+                (len lenlenn-1)
+                (len(len lenlenn-1))))])))
+
+;; Tests
+(check-expect (lenlen 123456) 3643889)
+(check-expect (lenlen 123457) 3643919)
+(check-expect (lenlen 2) 6)
+(check-expect (lenlen 3) 11)

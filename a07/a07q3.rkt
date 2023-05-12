df594690685fb04797f8b156510d6dcb1e86dacd
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-12 00:31:21 -0400

    Update a07q3.rkt

--- a07/a07q3.rkt
+++ a07/a07q3.rkt
@@ -1,11 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname a07q3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; ******************************************
-;;   Ekansh Lakhyani (20945086)
-;;   CS 115 Fall 2021
-;;   Assignment 07, Question 3
-;; ******************************************
+
 ;;New version
 
 

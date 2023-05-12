dc4f18a24941421b98a3f16446f0a7b548bbc5b1
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-12 00:31:56 -0400

    Update a08q3.rkt

--- a08/a08q3.rkt
+++ a08/a08q3.rkt
@@ -1,13 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname a08q3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;;
-;; ******************************************
-;;   Ekansh Lakhyani (20945086)
-;;   CS 115 Fall 2021
-;;   Assignment 08, Question 3
-;; ******************************************
-;;
 
 
 (define-struct course (code number grades))

0df4fc70c803390f323b8e708094a89a4e64c066
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-12 00:31:00 -0400

    Update a07q2.rkt

--- a07/a07q2.rkt
+++ a07/a07q2.rkt
@@ -1,12 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname a07q2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; ******************************************
-;;   Ekansh Lakhyani (20945086)
-;;   CS 115 Fall 2021
-;;   Assignment 07, Question 2
-;; ******************************************
-;;
 
 
 ;; (lenlen n) produces the LenLen number consuming a natural number n. The

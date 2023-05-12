0f6b2a408eea41b295cc0d7bbd1a2209dd92f1a1
Ekansh Lakhyani <103509498+ekanshlakhyani@users.noreply.github.com>
2023-05-12 00:27:53 -0400

    Update a02q3.rkt

--- a02/a02q3.rkt
+++ a02/a02q3.rkt
@@ -2,11 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname a02q3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
 
-;; ******************************************
-;;   Ekansh Lakhyani (20945086)
-;;   CS 115 Fall 2021
-;;   Assignment 02, Question 3
-;; ******************************************
+
 
 
 (define bloc-string "Bloc Québécois")
